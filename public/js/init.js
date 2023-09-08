36168219e98412daf208e30779526d7d75610aca
Gabriel <matiascoria427@gmail.com>
2023-09-08 16:01:05 -0300

    agrego eventos carrito y JQUERY

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,7 @@
 import { IndexRender } from './container/index.js'
+import { buttonCartEvent } from './services/eventCartService.js';
+
 window.onload = () => {
     IndexRender();
+    buttonCartEvent();
 }
